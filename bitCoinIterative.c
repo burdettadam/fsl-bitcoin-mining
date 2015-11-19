11783366a8dab9712ed02c4b6193f58a98da862f
Adam Burdett <burdettadam@gmail.com>
2015-11-19 10:48:19 -0700

    iterative

--- bitCoinIterative.c
+++ bitCoinIterative.c
@@ -20,7 +20,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <omp.h>
  
 // this is the block header, it is 80 bytes long (steal this code)
 typedef struct block_header {
@@ -94,74 +93,69 @@ void byte_swap(unsigned char* data, int len) {
 }
  
 int main() {
-        // start with a block header struct
-        block_header header;
-       
-        // we need a place to store the checksums
-        unsigned char hash1[SHA256_DIGEST_LENGTH];
-        unsigned char hash2[SHA256_DIGEST_LENGTH];
-       
-        // you should be able to reuse these, but openssl sha256 is slow, so your probbally not going to implement this anyway
+    // start with a block header struct
+    block_header header;
+   
+    // we need a place to store the checksums
+    unsigned char hash1[SHA256_DIGEST_LENGTH];
+    unsigned char hash2[SHA256_DIGEST_LENGTH];
+   
+    // you should be able to reuse these, but openssl sha256 is slow, so your probbally not going to implement this anyway
     SHA256_CTX sha256_pass1, sha256_pass2;
- 
- 
-        // we are going to supply the block header with the values from the generation block 0
-        header.version =        2;
-        hex2bin(header.prev_block,              "000000000000000117c80378b8da0e33559b5997f2ad55e2f7d18ec1975b9717");
-        hex2bin(header.merkle_root,             "871714dcbae6c8193a2bb9b2a69fe1c0440399f38d94b3a0f1b447275a29978a");
-        header.timestamp =      1392872245;
-        header.bits =           419520339;
-        header.nonce =          0;
-       
-        // the endianess of the checksums needs to be little, this swaps them form the big endian format you normally see in block explorer
-        byte_swap(header.prev_block, 32); // we need this to set up the header
-        byte_swap(header.merkle_root, 32); // we need this to set up the header
-       
-        // dump out some debug data to the terminal
-        printf("sizeof(block_header) = %d\n", sizeof(block_header));
-        printf("Block header (in human readable hexadecimal representation): ");
-        hexdump((unsigned char*)&header, sizeof(block_header));
-        double start = When();
-        double timer = start;
-        unsigned int counter =0;
-        #pragma omp parallel private(&header){
-    while ( timer < 60.0){
 
-        #pragma omp critical {
-            header.nonce = counter;
-            counter ++; 
-            if ( counter % 800000 == 0){
+
+    // we are going to supply the block header with the values from the generation block 0
+    header.version =        2;
+    hex2bin(header.prev_block,              "000000000000000117c80378b8da0e33559b5997f2ad55e2f7d18ec1975b9717");
+    hex2bin(header.merkle_root,             "871714dcbae6c8193a2bb9b2a69fe1c0440399f38d94b3a0f1b447275a29978a");
+    header.timestamp =      1392872245;
+    header.bits =           419520339;
+    header.nonce =          0;
+   
+    // the endianess of the checksums needs to be little, this swaps them form the big endian format you normally see in block explorer
+    byte_swap(header.prev_block, 32); // we need this to set up the header
+    byte_swap(header.merkle_root, 32); // we need this to set up the header
+   
+    // dump out some debug data to the terminal
+    printf("sizeof(block_header) = %d\n", sizeof(block_header));
+    printf("Block header (in human readable hexadecimal representation): ");
+    hexdump((unsigned char*)&header, sizeof(block_header));
+    double start = When();
+    double timer = When() - start;
+        while ( timer < 60.0){
+
+            // Use SSL's sha256 functions, it needs to be initialized
+            SHA256_Init(&sha256_pass1);
+            // then you 'can' feed data to it in chuncks, but here were just making one pass cause the data is so small
+            SHA256_Update(&sha256_pass1, (unsigned char*)&header, sizeof(block_header));
+            // this ends the sha256 session and writes the checksum to hash1
+            SHA256_Final(hash1, &sha256_pass1);
+               
+                // to display this, we want to swap the byte order to big endian
+         //       byte_swap(hash1, SHA256_DIGEST_LENGTH); // this is for printing 
+         //       printf("Useless First Pass Checksum: ");
+         //       hexdump(hash1, SHA256_DIGEST_LENGTH);
+         
+                // but to calculate the checksum again, we need it in little endian, so swap it back
+         //       byte_swap(hash1, SHA256_DIGEST_LENGTH);
+               
+            //same as above
+            SHA256_Init(&sha256_pass2);
+            SHA256_Update(&sha256_pass2, hash1, SHA256_DIGEST_LENGTH);
+            SHA256_Final(hash2, &sha256_pass2);
+            if ( header.nonce == 0 || header.nonce == 3 || header.nonce == 856192328 ) {
+                byte_swap(hash2, SHA256_DIGEST_LENGTH);
+                printf("Target Second Pass Checksum: ");
+                hexdump(hash2, SHA256_DIGEST_LENGTH);
+
+            }
+
+            header.nonce ++;
+            if ( header.nonce % 800000 == 0){
                 timer = (When() - start);
             }
         }
-        // Use SSL's sha256 functions, it needs to be initialized
-        SHA256_Init(&sha256_pass1);
-        // then you 'can' feed data to it in chuncks, but here were just making one pass cause the data is so small
-        SHA256_Update(&sha256_pass1, (unsigned char*)&header, sizeof(block_header));
-        // this ends the sha256 session and writes the checksum to hash1
-        SHA256_Final(hash1, &sha256_pass1);
-           
-            // to display this, we want to swap the byte order to big endian
-     //       byte_swap(hash1, SHA256_DIGEST_LENGTH); // this is for printing 
-     //       printf("Useless First Pass Checksum: ");
-     //       hexdump(hash1, SHA256_DIGEST_LENGTH);
-     
-            // but to calculate the checksum again, we need it in little endian, so swap it back
-     //       byte_swap(hash1, SHA256_DIGEST_LENGTH);
-           
-        //same as above
-        SHA256_Init(&sha256_pass2);
-        SHA256_Update(&sha256_pass2, hash1, SHA256_DIGEST_LENGTH);
-        SHA256_Final(hash2, &sha256_pass2);
-        if ( header.nonce == 0 || header.nonce == 3 || header.nonce == 856192328 ) {
-            byte_swap(hash2, SHA256_DIGEST_LENGTH);
-            printf("Target Second Pass Checksum: ");
-            hexdump(hash2, SHA256_DIGEST_LENGTH);
-
-        }
-    }
-}
-       printf("number of hashs per second = %f\n",counter / 60.0 );
+       printf("number of hashs per second = %f\n",header.nonce / 60.0 );
 
  
         return 0;
