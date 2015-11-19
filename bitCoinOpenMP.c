7d822a8010cc990532a20ef00eb96d583e85328d
Wesley Claiborne <wclaibor@gmail.com>
2015-11-19 11:17:10 -0700

    omp fixes

--- bitCoinOpenMP.c
+++ bitCoinOpenMP.c
@@ -104,30 +104,28 @@ int main() {
     // you should be able to reuse these, but openssl sha256 is slow, so your probbally not going to implement this anyway
     SHA256_CTX sha256_pass1, sha256_pass2;
 
-
-    // we are going to supply the block header with the values from the generation block 0
-    header.version =        2;
-    hex2bin(header.prev_block,              "000000000000000117c80378b8da0e33559b5997f2ad55e2f7d18ec1975b9717");
-    hex2bin(header.merkle_root,             "871714dcbae6c8193a2bb9b2a69fe1c0440399f38d94b3a0f1b447275a29978a");
-    header.timestamp =      1392872245;
-    header.bits =           419520339;
-    header.nonce =          0;
-   
-    // the endianess of the checksums needs to be little, this swaps them form the big endian format you normally see in block explorer
-    byte_swap(header.prev_block, 32); // we need this to set up the header
-    byte_swap(header.merkle_root, 32); // we need this to set up the header
-   
-    // dump out some debug data to the terminal
-    printf("sizeof(block_header) = %d\n", sizeof(block_header));
-    printf("Block header (in human readable hexadecimal representation): ");
     hexdump((unsigned char*)&header, sizeof(block_header));
     double start = When();
     double timer = When() - start;
     unsigned int counter =0;
     #pragma omp parallel private(header)
     {
+        // we are going to supply the block header with the values from the generation block 0
+        header.version =        2;
+        hex2bin(header.prev_block,              "000000000000000117c80378b8da0e33559b5997f2ad55e2f7d18ec1975b9717");
+        hex2bin(header.merkle_root,             "871714dcbae6c8193a2bb9b2a69fe1c0440399f38d94b3a0f1b447275a29978a");
+        header.timestamp =      1392872245;
+        header.bits =           419520339;
+        header.nonce =          0;
+       
+        // the endianess of the checksums needs to be little, this swaps them form the big endian format you normally see in block explorer
+        byte_swap(header.prev_block, 32); // we need this to set up the header
+        byte_swap(header.merkle_root, 32); // we need this to set up the header
+       
+        // dump out some debug data to the terminal
+        //printf("sizeof(block_header) = %d\n", sizeof(block_header));
+        //printf("Block header (in human readable hexadecimal representation): ");
         while ( timer < 60.0){
-
             #pragma omp critical 
             {
                 header.nonce = counter;
@@ -135,6 +133,10 @@ int main() {
                 if ( counter % 800000 == 0){
                     timer = (When() - start);
                 }
+            }
+            if(header.nonce == 0) {
+                printf("compare to last printout: ");
+                hexdump((unsigned char*)&header, sizeof(block_header));
             }
             // Use SSL's sha256 functions, it needs to be initialized
             SHA256_Init(&sha256_pass1);
@@ -157,7 +159,7 @@ int main() {
             SHA256_Final(hash2, &sha256_pass2);
             if ( header.nonce == 0 || header.nonce == 3 || header.nonce == 856192328 ) {
                 byte_swap(hash2, SHA256_DIGEST_LENGTH);
-                printf("Target Second Pass Checksum: ");
+                printf("nonce = %d: \n", header.nonce);
                 hexdump(hash2, SHA256_DIGEST_LENGTH);
 
             }
