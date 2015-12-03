8ab735efd51e68e4f044582cc4d866d00b11ccdc
Wesley Claiborne <wclaibor@gmail.com>
2015-12-03 09:59:53 -0700

    Cuda implementation

---
+++ bitCoinCUDA.cu
@@ -0,0 +1,207 @@
+#ifdef fail
+        #!/bin/bash
+        # NOTE you can chmod 0755 this file and then execute it to compile (or just copy and paste)
+        gcc -o hashblock hashblock.c -lssl
+        exit 0
+#endif
+ 
+//#include <openssl/sha.h>
+#include "sha2.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+ 
+// this is the block header, it is 80 bytes long (steal this code)
+typedef struct block_header {
+        unsigned int    version;
+        // dont let the "char" fool you, this is binary data not the human readable version
+        unsigned char   prev_block[32];
+        unsigned char   merkle_root[32];
+        unsigned int    timestamp;
+        unsigned int    bits;
+        unsigned int    nonce;
+} block_header;
+ 
+ __device__ unsigned char* headerChanger(block_header header){
+    unsigned char ch[2];
+    memcpy(ch, (char*)&header.version, sizeof(int));
+    unsigned char results[70];
+    results[0] = ch[0];
+    results[1] = ch[1];
+    int i;
+    for( i = 0 ; i < 32; i++){
+        results[i+2] = header.prev_block[i];
+    }
+    for( i = 0 ; i < 32; i++){
+        results[i+34] = header.merkle_root[i];
+    }
+    memcpy(ch, (char*)&header.timestamp, sizeof(int));
+    results[64] =  ch[0];
+    results[65] =  ch[1];
+
+    memcpy(ch, (char*)&header.bits, sizeof(int));
+    results[66] =  ch[0];
+    results[67] =  ch[1];
+
+    memcpy(ch, (char*)&header.nonce, sizeof(int));
+    results[68] =  ch[0];
+    results[69] =  ch[1];
+    return results;
+ }
+
+double When()
+{
+    struct timeval tp;
+    gettimeofday(&tp, NULL);
+    return ((double) tp.tv_sec + (double) tp.tv_usec * 1e-6);
+}
+// we need a helper function to convert hex to binary, this function is unsafe and slow, but very readable (write something better)
+__device__ void hex2bin(unsigned char* dest, unsigned char* src)
+{
+        unsigned char bin;
+        int c, pos;
+        char buf[3];
+ 
+        pos=0;
+        c=0;
+        buf[2] = 0;
+        while(c < strlen(src))
+        {
+                // read in 2 characaters at a time
+                buf[0] = src[c++];
+                buf[1] = src[c++];
+                // convert them to a interger and recast to a char (uint8)
+                dest[pos++] = (unsigned char)strtol(buf, NULL, 16);
+        }
+       
+}
+ 
+// this function is mostly useless in a real implementation, were only using it for demonstration purposes
+__device__ void hexdump(unsigned char* data, int len)
+{
+        int c;
+       
+        c=0;
+        while(c < len)
+        {
+                printf("%.2x", data[c++]);
+        }
+        printf("\n");
+}
+ 
+// this function swaps the byte ordering of binary data, this code is slow and bloated (write your own)
+__device__ void byte_swap(unsigned char* data, int len) {
+        int c;
+        unsigned char tmp[len];
+       swap256(,data);
+
+
+        c=0;
+        while(c<len)
+        {
+                tmp[c] = data[len-(c+1)];
+                c++;
+        }
+       
+        c=0;
+        while(c<len)
+        {
+                data[c] = tmp[c];
+                c++;
+        }
+}
+
+__global__ doCalc(block_header *header, int seed) {
+    block_header header;
+   
+    // we need a place to store the checksums
+    unsigned char hash1[SHA256_DIGEST_SIZE];
+    unsigned char hash2[SHA256_DIGEST_SIZE];
+   
+    // you should be able to reuse these, but openssl sha256 is slow, so your probbally not going to implement this anyway
+    sha256_ctx sha256_pass1, sha256_pass2;
+
+
+    // we are going to supply the block header with the values from the generation block 0
+    header.version =        2;
+    hex2bin(header.prev_block,              "000000000000000117c80378b8da0e33559b5997f2ad55e2f7d18ec1975b9717");
+    hex2bin(header.merkle_root,             "871714dcbae6c8193a2bb9b2a69fe1c0440399f38d94b3a0f1b447275a29978a");
+    header.timestamp =      1392872245;
+    header.bits =           419520339;
+
+    header.nonce = seed * blockDim.x + threadIdx.x;
+    // Use SSL's sha256 functions, it needs to be initialized
+    sha256_init(&sha256_pass1);
+    // then you 'can' feed data to it in chuncks, but here were just making one pass cause the data is so small
+    sha256_update(&sha256_pass1, (unsigned char*)&header, sizeof(block_header));
+    // this ends the sha256 session and writes the checksum to hash1
+    sha256_final(hash1, &sha256_pass1);
+       
+        // to display this, we want to swap the byte order to big endian
+ //       byte_swap(hash1, SHA256_DIGEST_LENGTH); // this is for printing 
+ //       printf("Useless First Pass Checksum: ");
+ //       hexdump(hash1, SHA256_DIGEST_LENGTH);
+ 
+        // but to calculate the checksum again, we need it in little endian, so swap it back
+ //       byte_swap(hash1, SHA256_DIGEST_LENGTH);
+       
+    //same as above
+    sha256_init(&sha256_pass2);
+    sha256_update(&sha256_pass2, hash1, SHA256_DIGEST_SIZE);
+    sha256_final(hash2, &sha256_pass2);
+    if ( header.nonce == 0 || header.nonce == 3 || header.nonce == 856192328 ) {
+        byte_swap(hash2, SHA256_DIGEST_SIZE);
+        printf("Target Second Pass Checksum: ");
+        hexdump(hash2, SHA256_DIGEST_SIZE);
+
+    }
+}
+
+int main() {
+    int blocksize = 1024;
+    int threads = 1024;
+
+    // start with a block header struct
+    block_header header;
+   
+    // we need a place to store the checksums
+    unsigned char hash1[SHA256_DIGEST_SIZE];
+    unsigned char hash2[SHA256_DIGEST_SIZE];
+   
+    // you should be able to reuse these, but openssl sha256 is slow, so your probbally not going to implement this anyway
+    sha256_ctx sha256_pass1, sha256_pass2;
+
+
+    // we are going to supply the block header with the values from the generation block 0
+    header.version =        2;
+    hex2bin(header.prev_block,              "000000000000000117c80378b8da0e33559b5997f2ad55e2f7d18ec1975b9717");
+    hex2bin(header.merkle_root,             "871714dcbae6c8193a2bb9b2a69fe1c0440399f38d94b3a0f1b447275a29978a");
+    header.timestamp =      1392872245;
+    header.bits =           419520339;
+    header.nonce =          0;
+   
+    // the endianess of the checksums needs to be little, this swaps them form the big endian format you normally see in block explorer
+    byte_swap(header.prev_block, 32); // we need this to set up the header
+    byte_swap(header.merkle_root, 32); // we need this to set up the header
+   
+    // dump out some debug data to the terminal
+    printf("sizeof(block_header) = %d\n", sizeof(block_header));
+    printf("Block header (in human readable hexadecimal representation): ");
+    hexdump((unsigned char*)&header, sizeof(block_header));
+    double start = When();
+    double timer = When() - start;
+    int hashes = 0;
+
+    int counter = 0;
+    while ( timer < 60.0){
+        docalc<<< blocksize, threads >>>(header, counter);
+        hashes += bocksize*threads;
+        counter++;
+        timer = start - When();
+    }
+    cudaDeviceSynchronize();
+    printf("number of hashs per second = %f\n",hashes / start - When() );
+
+ 
+    return 0;
+}
