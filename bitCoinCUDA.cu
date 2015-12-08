59343ec5f956864077c204092e50c11376078859
Wesley Claiborne <wclaibor@gmail.com>
2015-12-08 12:10:29 -0700

    cuda fixes

--- bitCoinCUDA.cu
+++ bitCoinCUDA.cu
@@ -12,7 +12,7 @@
 #include <sys/time.h>
 #include "sha256.cu"
  
-#define SHA256_DIGEST_SIZE 64
+#define SHA256_DIGEST_SIZE 32
 #define NUM_BLOCKS 1024
 
 // this is the block header, it is 80 bytes long (steal this code)
@@ -54,16 +54,12 @@ void hex2bin(unsigned char* dest, const char* src)
 }
  
 // this function is mostly useless in a real implementation, were only using it for demonstration purposes
-__device__ void hexdump(unsigned char* data, int len)
+__device__ void print_hash(unsigned char hash[])
 {
-        int c;
-       
-        c=0;
-        while(c < len)
-        {
-                printf("%.2x", data[c++]);
-        }
-        printf("\n");
+   int idx;
+   for (idx=0; idx < 32; idx++)
+      printf("%02x",hash[idx]);
+   printf("\n");
 }
  
 // this function swaps the byte ordering of binary data, this code is slow and bloated (write your own)
@@ -86,7 +82,16 @@ __device__ void byte_swap(unsigned char* data) {
         }
 }
 
-__global__ void doCalc(block_header header, int seed) {
+__global__ void doCalc(unsigned char *dev_merkle_root, unsigned char *dev_prev_block, int seed) {
+    int i;   
+    block_header header;
+    header.version =        2;
+    header.timestamp =      1392872245;
+    header.bits =           419520339;
+    for(i=0;i<32;i++) {
+        header.prev_block[i] = dev_prev_block[i];
+        header.merkle_root[i] = dev_merkle_root[i];
+    }
 
     // we need a place to store the checksums
     unsigned char hash1[SHA256_DIGEST_SIZE];
@@ -96,8 +101,7 @@ __global__ void doCalc(block_header header, int seed) {
     SHA256_CTX sha256_pass1, sha256_pass2;
 
     header.nonce = (seed *  blockDim.x * NUM_BLOCKS) + blockIdx.x * blockDim.x + threadIdx.x;
-    printf("test\n");
-    printf("nonce: %u\n", header.nonce);
+    //if(threadIdx.x == 0) printf("nonce: %d\n", header.nonce);
     // Use SSL's sha256 functions, it needs to be initialized
     sha256_init(&sha256_pass1);
     // then you 'can' feed data to it in chuncks, but here were just making one pass cause the data is so small
@@ -118,15 +122,16 @@ __global__ void doCalc(block_header header, int seed) {
     sha256_update(&sha256_pass2, hash1, SHA256_DIGEST_SIZE);
     sha256_final(&sha256_pass2, hash2);
     if ( header.nonce == 0 || header.nonce == 3 || header.nonce == 856192328 ) {
-        hexdump((unsigned char*)&header, sizeof(block_header));
-        printf("u\n", header.nonce);
+        //hexdump((unsigned char*)&header, sizeof(block_header));
+        printf("%u:\n", header.nonce);
         byte_swap(hash2);
         printf("Target Second Pass Checksum: \n");
-        hexdump(hash2, SHA256_DIGEST_SIZE);
+        print_hash(hash2);
     }
 }
 
 int main() {
+    int i = 0;
     int blocksize = 1024;
     int threads = 1024;
 
@@ -134,20 +139,22 @@ int main() {
 
     int counter = 0;
 
-    block_header header;
-
+    unsigned char prev_block[32], dev_prev_block[32], merkle_root[32], dev_merkle_root[32];
 
     // we are going to supply the block header with the values from the generation block 0
-    header.version =        2;
-    hex2bin(header.prev_block,              "000000000000000117c80378b8da0e33559b5997f2ad55e2f7d18ec1975b9717");
-    hex2bin(header.merkle_root,             "871714dcbae6c8193a2bb9b2a69fe1c0440399f38d94b3a0f1b447275a29978a");
-    header.timestamp =      1392872245;
-    header.bits =           419520339;
+    hex2bin(prev_block,              "000000000000000117c80378b8da0e33559b5997f2ad55e2f7d18ec1975b9717");
+    hex2bin(merkle_root,             "871714dcbae6c8193a2bb9b2a69fe1c0440399f38d94b3a0f1b447275a29978a");
+
+    cudaMalloc((void**)&dev_prev_block, 32*sizeof(unsigned char));
+    cudaMemcpy(dev_prev_block, &(prev_block[0]), 32 * sizeof(unsigned char), cudaMemcpyHostToDevice);
+
+    cudaMalloc((void**)&dev_merkle_root, 32*sizeof(unsigned char));
+    cudaMemcpy(dev_merkle_root, &(merkle_root[0]), 32 * sizeof(unsigned char), cudaMemcpyHostToDevice);
 
     double start = When();
     double timer = When() - start;
     while ( timer < 60.0){
-        doCalc<<< blocksize, threads >>>(header, counter);
+        doCalc<<< blocksize, threads >>>(dev_prev_block, dev_merkle_root, counter);
         hashes += blocksize*threads;
         counter++;
         timer = When() - start;
